6d0714eb147d881043431944c1dc33af725fe778
PROGRAMACION <PROGRAMACION@DESKTOP-L4F2K7S.localdomain>
2025-04-25 12:33:12 +0200

    segunda version de la app

--- David_Rancaño_Ibáñez_Git_Hub/src/ph01/GitHuBPracticaa25abril.java
+++ David_Rancaño_Ibáñez_Git_Hub/src/ph01/GitHuBPracticaa25abril.java
@@ -9,10 +9,12 @@ public class GitHuBPracticaa25abril {
 		int num2 = 5; 
 		int suma = num1 + num2;
 		int resta = num1 - num2;
+		int multiplicacion = num1 * num2;
 		
 		
 		System.out.println("La suma de los numeros es: " + suma);
 		System.out.println("La resta de los numeros es: " + resta);
+		System.out.println("La multiplicacion de los numeros es: " + multiplicacion);
 		
 		
 		
